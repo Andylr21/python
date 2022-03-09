4d5a54539e6b513801ff7a14ef86c690340797c3
Andylr21 <100741455+Andylr21@users.noreply.github.com>
2022-03-08 19:24:31 -0600

    Add files via upload

---
+++ Arreglos.py
@@ -0,0 +1,25 @@
+# arreglos: Colección de valores de uno o más tipos de datos
+# SINTAXIS:
+# nombre_del_arreglo = [valores]
+arr_numeros = [10,4,2,14]
+
+arreglo   =   [1,2,3,4, True, "Eduardo"]
+
+#Index (indice) 0 1 2 3
+
+lista = [2,3,6,5, False, "Andrea"]
+#Index (indice) 0 1 2 3
+#Operacioens con arreglos
+#Modificar un elemento del arreglo
+
+arreglo[0]= "Cambio de arreglo"
+lista[0] = "Cambio lista"
+
+#Agregar un elemetno a un arreglo o lista
+arreglo.append("Otro elemento arreglo")
+lista.append("Otro elemento lista")
+
+print(arreglo, type(arreglo)) # type imprimo el tipo de dato de la variable 
+print(lista)
+
+print(arr_numeros)
---
+++ Ejercicio11.py
@@ -0,0 +1,9 @@
+# Hacer una función para que imprima un arreglo o lista de 10 elementos numéricos
+
+arreglo    = [1,2,3,4,5,6,7,8,9,10]
+
+#Operacioens con arreglos 
+
+arreglo[5]= 12
+
+print(arreglo, type(arreglo))
